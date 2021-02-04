10b9342517d8c3579e49a61867350eb0438c78bd
lucas-cmh <lucas_santoscaminha@hotmail.com>
2021-02-04 08:17:48 -0300

    adicionado cacheamento na consulta de Topicos

--- src/main/java/br/com/caminha/forum/ForumApplication.java
+++ src/main/java/br/com/caminha/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package br.com.caminha.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport // Para o Spring acessar a Paginacao e Ordenação
+@EnableCaching // Habilita o uso de Caching na aplicação
 public class ForumApplication {
 
 	public static void main(String[] args) {
