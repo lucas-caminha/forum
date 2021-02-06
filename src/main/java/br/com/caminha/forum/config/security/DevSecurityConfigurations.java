8307c1e677f46a98d073db6c7d5f529283a39c48
lucas-cmh <lucas_santoscaminha@hotmail.com>
2021-02-06 13:10:13 -0300

    criacao de profiles para determinar acessos ao servidor

---
+++ src/main/java/br/com/caminha/forum/config/security/DevSecurityConfigurations.java
@@ -0,0 +1,27 @@
+package br.com.caminha.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	
+	// Configuração de Autorização (A determinada Url por determinado Usuário)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/**").permitAll()
+				.anyRequest().authenticated()
+				.and().csrf().disable();
+	}
+	
+	
+}
+	
